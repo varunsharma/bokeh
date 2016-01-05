3ccf4f17cd08a510e9206b6f4fe7f0d624fdccb2
bcollins <bcollins@continuum.io>
2016-01-05 15:28:33 -0600

    swapped out stamen providers for ssl versions

--- bokeh/tile_providers.py
+++ bokeh/tile_providers.py
@@ -3,31 +3,32 @@
 from .models.tiles import WMTSTileSource
 
 STAMEN_TONER = WMTSTileSource(
-    url='http://tile.stamen.com/toner/{Z}/{X}/{Y}.png', 
+    url='https://stamen-tiles.a.ssl.fastly.net/toner/{Z}/{X}/{Y}.png',
     attribution=(
         'Map tiles by <a href="http://stamen.com">Stamen Design</a>, '
-        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.' 
+        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.'
         'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, '
         'under <a href="http://www.openstreetmap.org/copyright">ODbL</a>'
-    )  
+    )
 )
 
+
 STAMEN_TONER_LABELS = WMTSTileSource(
-    url='http://tile.stamen.com/toner-labels/{Z}/{X}/{Y}.png', 
+    url='https://stamen-tiles.a.ssl.fastly.net/toner-labels/{Z}/{X}/{Y}.png',
     attribution=(
         'Map tiles by <a href="http://stamen.com">Stamen Design</a>, '
-        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.' 
+        'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>.'
         'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, '
         'under <a href="http://www.openstreetmap.org/copyright">ODbL</a>'
-    )  
+    )
 )
 
 STAMEN_TERRAIN = WMTSTileSource(
-    url='http://tile.stamen.com/terrain/{Z}/{X}/{Y}.png', 
+    url='https://stamen-tiles.a.ssl.fastly.net/terrain/{Z}/{X}/{Y}.png',
     attribution=(
         'Map tiles by <a href="http://stamen.com">Stamen Design</a>, '
         'under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. '
         'Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under '
         '<a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.'
-    )  
+    )
 )
